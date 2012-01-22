3cd0bbc29e45c1cecb8f871bb4c0256d6b5e0499
Derek Kastner <dkastner@gmail.com>
2012-01-21 19:44:17 -0500

    Dynamics and links for home/layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title
+    title = case controller.class
+    when HighVoltage::PagesController then
+      params[:id]
+    end
+    title ||= 'Welcome'
+    title.capitalize
+  end
 end
