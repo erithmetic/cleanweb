bfa4d2e4fabb2c6a27e937eec8668c934ada3ad9
Derek Kastner <dkastner@gmail.com>
2012-01-21 23:07:00 -0500

    Revert "Omniauth config"
    This reverts commit a3fc967feeb40a0b9349fbc8d81f805c72730a46.

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
-    {:scope => 'email, offline_access',:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
-end
