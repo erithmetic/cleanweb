a3fc967feeb40a0b9349fbc8d81f805c72730a46
Derek Kastner <dkastner@gmail.com>
2012-01-21 22:58:02 -0500

    Omniauth config

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
+    {:scope => 'email, offline_access',:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+end
